711bd6b4f66d555fbfc6aeddf924ad096f11e645
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-07-01 00:15:31 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,135 @@
+const data = [
+  { originalStr: "TBA", encryptedStr: "GynU" },
+  { originalStr: "SLf", encryptedStr: "HCfz" },
+  { originalStr: "SFM", encryptedStr: "HA34" },
+  { originalStr: "RIH", encryptedStr: "HSLd" },
+  { originalStr: "MAY", encryptedStr: "AirM" },
+  { originalStr: "Mdy", encryptedStr: "Ag/s" },
+  { originalStr: "SMMI", encryptedStr: "HCbYWQ==" },
+  { originalStr: "SMMA", encryptedStr: "HCbYUQ==" },
+  { originalStr: "NdA", encryptedStr: "AQ/U" },
+  { originalStr: "SvA", encryptedStr: "HB3U" },
+  { originalStr: "TnS1", encryptedStr: "GwXGIQ==" },
+  { originalStr: "RjP", encryptedStr: "HQHF" },
+  { originalStr: "MTN", encryptedStr: "Ih/7" },
+  { originalStr: "MTN2", encryptedStr: "Ah/7Ig==" },
+  { originalStr: "NS", encryptedStr: "ATg=" },
+  { originalStr: "SFT", encryptedStr: "HA3B" },
+  { originalStr: "MsRh", encryptedStr: "AjjHWA==" },
+  { originalStr: "ZHO", encryptedStr: "FSPa" },
+  { originalStr: "MRH1", encryptedStr: "AjndIQ==" },
+  { originalStr: "IAH", encryptedStr: "Bird" },
+  { originalStr: "MSK1", encryptedStr: "AjjeIQ==" },
+  { originalStr: "AZZ", encryptedStr: "DhHv" },
+  { originalStr: "HAI", encryptedStr: "Byrc" },
+  { originalStr: "MMA1", encryptedStr: "AibUIQ==" },
+  { originalStr: "SZZ", encryptedStr: "HBHv" },
+  { originalStr: "BMH", encryptedStr: "DSbd" },
+  { originalStr: "RRn", encryptedStr: "HTn7" },
+  { originalStr: "SAK1", encryptedStr: "HCreIQ==" },
+  { originalStr: "ABH1", encryptedStr: "DindlQ==" },
+  { originalStr: "KMM", encryptedStr: "BCbY" },
+  { originalStr: "EZm", encryptedStr: "ChHy" },
+  { originalStr: "SfT", encryptedStr: "HA3B" },
+  { originalStr: "SUT", encryptedStr: "HB7B" },
+  { originalStr: "STS1", encryptedStr: "HD/GIQ==" },
+  { originalStr: "NJb", encryptedStr: "ASH3" },
+  { originalStr: "HYK1", encryptedStr: "BxLeIQ==" },
+  { originalStr: "MZD", encryptedStr: "AhHx" },
+  { originalStr: "AAR", encryptedStr: "DirH" },
+  { originalStr: "KIS", encryptedStr: "BCLG" },
+  { originalStr: "SZn", encryptedStr: "HDH7" },
+  { originalStr: "NM", encryptedStr: "ASY=" },
+  { originalStr: "SbS", encryptedStr: "HAnG" },
+  { originalStr: "BSq", encryptedStr: "DTjk" },
+  { originalStr: "RSA", encryptedStr: "HRjU" },
+  { originalStr: "AYM", encryptedStr: "DhL4" },
+  { originalStr: "NPN", encryptedStr: "ATv7" },
+  { originalStr: "LFR", encryptedStr: "AW3H" },
+  { originalStr: "TMN", encryptedStr: "Gwbb" },
+  { originalStr: "MZQ", encryptedStr: "AhHk" },
+  { originalStr: "Sye", encryptedStr: "HBLw" },
+  { originalStr: "SB1", encryptedStr: "HCmk" },
+  { originalStr: "ATI", encryptedStr: "Dj/c" },
+  { originalStr: "MNA2", encryptedStr: "HQ3UeA==" },
+  { originalStr: "KMRn", encryptedStr: "BCbHfg==" },
+  { originalStr: "NAH1", encryptedStr: "ASrdIQ==" },
+  { originalStr: "SCG", encryptedStr: "HCjS" },
+  { originalStr: "RUH", encryptedStr: "HT7d" },
+  { originalStr: "MDN", encryptedStr: "Ag/h" },
+  { originalStr: "NRO", encryptedStr: "ATna" },
+  { originalStr: "FZd", encryptedStr: "AibHlw" },
+  { originalStr: "BUN", encryptedStr: "DT77" },
+  { originalStr: "Hrr", encryptedStr: "BxnH" },
+  { originalStr: "MABn", encryptedStr: "AirXfg==" },
+  { originalStr: "MDAH", encryptedStr: "AG/UWA==" },
+  { originalStr: "Nan", encryptedStr: "AQr7" },
+  { originalStr: "MMR5", encryptedStr: "AibHJQ==" },
+  { originalStr: "ZUM", encryptedStr: "FT7Y" },
+  { originalStr: "MTO", encryptedStr: "Aj/a" },
+  { originalStr: "SQU", encryptedStr: "HDrA" },
+  { originalStr: "LtB", encryptedStr: "Ax/X" },
+  { originalStr: "Mar2", encryptedStr: "AirHlg==" },
+  { originalStr: "PyN", encryptedStr: "HxLb" },
+  { originalStr: "MH1", encryptedStr: "AiOk" },
+  { originalStr: "ABS", encryptedStr: "DgnG" },
+  { originalStr: "AuR", encryptedStr: "Dh7H" },
+  { originalStr: "MHK3", encryptedStr: "AiPeIw==" },
+  { originalStr: "UMM", encryptedStr: "GibY" },
+  { originalStr: "SK", encryptedStr: "HCA" },
+  { originalStr: "FDW", encryptedStr: "CQ/i" },
+  { originalStr: "SwK", encryptedStr: "HBz+" },
+  { originalStr: "SBW", encryptedStr: "Hcnc" },
+  { originalStr: "JER", encryptedStr: "BQ7H" },
+  { originalStr: "MRM2", encryptedStr: "AjnYIg==" },
+  { originalStr: "BBN", encryptedStr: "DQn7" },
+  { originalStr: "KKAr", encryptedStr: "BCDUYg==" },
+  { originalStr: "APV", encryptedStr: "Djvj" },
+  { originalStr: "ThR", encryptedStr: "GwPn" },
+  { originalStr: "TAA1", encryptedStr: "Gyr0IQ==" },
+  { originalStr: "AMK", encryptedStr: "Dgbe" },
+  { originalStr: "MTM", encryptedStr: "Ah/Y" },
+  { originalStr: "SPD", encryptedStr: "HDvx" },
+  { originalStr: "OMG", encryptedStr: "ACbS" },
+  { originalStr: "ISR", encryptedStr: "BhjH" },
+  { originalStr: "ZHn", encryptedStr: "FSP7" },
+  { originalStr: "LKS", encryptedStr: "AyDG" },
+];
+
+function decodeBase64(encodedStr) {
+  try {
+    return atob(encodedStr);
+  } catch (e) {
+    return null;
+  }
+}
+
+function decodeAndCompare() {
+  const encodedStr = document.getElementById("base64Input").value;
+  const decodedStr = decodeBase64(encodedStr);
+  let outputHtml = "";
+
+  if (decodedStr !== null) {
+    let found = false;
+
+    for (let i = 0; i < data.length; i++) {
+      const { originalStr, encryptedStr } = data[i];
+      if (encryptedStr === encodedStr) {
+        outputHtml = `
+                    <p class="text-green-700"><strong>Original String:</strong> ${originalStr}</p>
+                    <p class="text-blue-700"><strong>Decoded String:</strong> ${decodedStr}</p>
+                `;
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      outputHtml = `<p class="text-yellow-700">Decoded string does not match any original string in the list.</p>`;
+    }
+  } else {
+    outputHtml = `<p class="text-red-700">Invalid Base64 string</p>`;
+  }
+
+  document.getElementById("output").innerHTML = outputHtml;
+}
